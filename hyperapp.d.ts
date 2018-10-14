b7160816c6325ec9c0924b2c9233ca90d1996b4a
diontools <diontools@users.noreply.github.com>
2018-10-14 18:24:16 +0900

    improve type

--- hyperapp.d.ts
+++ hyperapp.d.ts
@@ -1,38 +1,42 @@
 declare module "hyperapp/src/index.js" {
-    export type Children = VNode | string | number | null
+  export type Children = VNode | string | number | null
     
-    export enum VNodeType {
-      DEFAULT = 0,
-      RECYCLED_NODE,
-      TEXT_NODE,
-    }
+  export enum VNodeType {
+    DEFAULT = 0,
+    RECYCLED_NODE,
+    TEXT_NODE,
+  }
     
-    export interface VNode<Props = {}> {
-      name: string,
-      props: Props,
-      children: Array<VNode>
-      element: Element,
-      key: string,
-      type: VNodeType
-    }
+  export interface VNode<Props = {}> {
+    name: string,
+    props: Props,
+    children: Array<VNode>
+    element: Element,
+    key: string,
+    type: VNodeType
+  }
     
-    /**
-     * Create a new virtual DOM node. A virtual DOM is a description of what a DOM should look like using a tree of virtual nodes.
-     * @param name The name of an Element or a function that returns a virtual DOM node.
-     * @param props HTML props, SVG props, DOM events, Lifecycle Events, and Keys.
-     * @param children The element's child nodes.
-     */
-    export function h<Props>(
-      name: string,
-      props?: Props | null,
-      ...children: Array<Children | Children[]>
-    ): VNode<Props>
+  /**
+   * Create a new virtual DOM node. A virtual DOM is a description of what a DOM should look like using a tree of virtual nodes.
+   * @param name The name of an Element or a function that returns a virtual DOM node.
+   * @param props HTML props, SVG props, DOM events, Lifecycle Events, and Keys.
+   * @param children The element's child nodes.
+   */
+  export function h<Props>(
+    name: string,
+    props?: Props | null,
+    ...children: Array<Children | Children[]>
+  ): VNode<Props>
 
-    export function app<State>(
-        props: {
-            init: State,
-            view: (state: State) => VNode,
-            container: Element
-        }
-      ): void
+  export type DispatchType = (obj: Function, data?: any) => void;
+
+  export function app<State>(
+    props: {
+      init: State,
+      view: (state: State) => VNode,
+      container: Element
+    }
+  ): {
+      dispatch: DispatchType
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,60 @@
-import { h, app } from 'hyperapp/src/index.js';
-type Actions<S, P = {}> = (state: S, props: P, ev: Event) => S;
+import { h, app, DispatchType } from 'hyperapp/src/index.js';
+type Action<S, P = {}> = (state: S, props: P, ev: Event) => S | [S, JSX.Element | object];
+type EffectRunner<P> = (props: P, dispatch: DispatchType) => void;
+type Effect<P> = (props: P) => {
+    [X in keyof P]: P[X];
+} & {
+    effect: EffectRunner<P>
+};
+
+
+interface DelayEffectProps {
+    action: any;
+    interval: number;
+}
+
+// Effect Runner
+const delayEffect: EffectRunner<DelayEffectProps> = (props, dispatch) =>
+    setTimeout(() => dispatch(props.action), props.interval);
+
+// Effect Constructor
+const delay: Effect<DelayEffectProps> = (props) => ({
+    effect: delayEffect,
+    ...props,
+});
+
 
 const mainState = {
     value: 0,
+    value2: 0,
 }
 
 type MainStateType = typeof mainState;
-type MainActions<P = {}> = Actions<MainStateType, P>;
+type MainAction<P = {}> = Action<MainStateType, P>;
 
-const IncrementBy: MainActions<{ num: number }> = (state, args) => ({ ...state, value: state.value + args.num });
-const Reset: MainActions = state => ({ value: 0 });
-
-function act<S, P>(tuple: [Actions<S, P>, P]) {
-    return tuple;
+// Type check function
+function act(state: MainStateType): any;
+function act<S, P>(action: Action<S, P>, props: P): any;
+function act(...args: any[]) {
+    return args.length == 1 ? args[0] : args;
 }
+
+const IncrementBy: MainAction<{ num: number }> = (state, args) => ({ ...state, value: state.value + args.num });
+const Reset: MainAction = state => ({ ...state, value: 0 });
+const DelayIncrementBy: MainAction<{ num: number, delay: number }> = (state, args) => [
+    state,
+    delay({ interval: args.delay, action: act(IncrementBy, { num: args.num }) }),
+];
+
 
 app({
     init: mainState,
     view: state => (
         <div>
-            <button onClick={act([IncrementBy, { num: 5 }])}>increment</button>
+            <button onClick={act(IncrementBy, { num: 5 })}>increment</button>
             <button onClick={Reset}>reset</button>
+            <button onClick={act({ ...state, value: 0 })}>reset2</button>
+            <button onClick={act(DelayIncrementBy, { num: 10, delay: 500 })}>delay increment</button>
             count: {state.value}
         </div>
     ),
