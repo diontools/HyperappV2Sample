e32048f6fc67ca33cf7ec901864faba88bbd3590
diontools <diontools@users.noreply.github.com>
2018-10-16 18:09:12 +0900

    tick count

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,29 @@ interface DelayEffectProps {
     interval: number;
 }
 
-// Effect Runner
+// delay Effect Runner
 const delayEffect: EffectRunner<DelayEffectProps> = (props, dispatch) =>
     setTimeout(() => dispatch(props.action), props.interval);
 
-// Effect Constructor
+// delay Effect Constructor
 const delay: Effect<DelayEffectProps, DelayEffectProps> = (props) => ({
     effect: delayEffect,
+    ...props,
+});
+
+
+interface TickEffectProps {
+    action: any;
+    interval: number;
+}
+
+const tickEffect: EffectRunner<TickEffectProps> = (props, dispatch) => {
+    const id = setInterval(() => dispatch(props.action), props.interval);
+    return () => clearInterval(id);
+};
+
+const tick: Effect<TickEffectProps, TickEffectProps> = (props) => ({
+    effect: tickEffect,
     ...props,
 });
 
@@ -19,6 +35,7 @@ const delay: Effect<DelayEffectProps, DelayEffectProps> = (props) => ({
 const mainState = {
     value: 0,
     value2: 0,
+    tickEnabled: false,
     count: 0,
 }
 
@@ -39,6 +56,11 @@ const DelayIncrementBy: MainAction<{ num: number, delay: number }> = (state, arg
     delay({ interval: args.delay, action: act(IncrementBy, { num: args.num }) }),
 ];
 
+const SetTickEnabled: MainAction<{ enable: boolean }> = (state, args, event) => {
+    event.preventDefault();
+    console.log(state, args, event);
+    return { ...state, tickEnabled: args.enable };
+};
 const CountUp: MainAction = state => { console.log('CountUp'); return { ...state, count: state.count + 1 } };
 
 app({
@@ -52,9 +74,17 @@ app({
             <button onClick={act(Reset)}>reset</button>
             <button onClick={act(DelayIncrementBy, { num: 10, delay: 500 })}>delay increment</button>
             <p>value: {state.value}</p>
-            <p>count: {state.count}</p>
+            <p>
+                <input type="checkbox" checked={state.tickEnabled} onClick={act(SetTickEnabled, { enable: !state.tickEnabled })}></input>
+                count: {state.count} {state.tickEnabled ? 't' : 'f'}
+            </p>
         </div>
     ),
     container: document.body,
-    subscriptions: state => { console.log('subs'); return [delay({ action: CountUp, interval: 100 })] },
+    subscriptions: state => {
+        console.log('subs');
+        return [
+            state.tickEnabled && tick({ action: CountUp, interval: 1000 })
+        ];
+    },
 });
